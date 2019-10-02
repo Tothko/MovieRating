c93d25b61985c83e8ba94aea172ab1676fbc5a3f
Tothko <43235034+Tothko@users.noreply.github.com>
2019-10-02 23:01:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using MovieRating.Core.MovieRating.Core;
+using MovieRating.Infrastructure.JSONReader;
+using System;
+
+namespace MovieRating
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            ReviewService RevServ = new ReviewService(new ReviewRepository());
+            
+            Console.WriteLine("Select user");
+            int cislo = RevServ.CountOfUserReviews(int.Parse(Console.ReadLine()));
+            Console.WriteLine("User has this many reviews" + cislo);
+        }
+    }
+}
+    
